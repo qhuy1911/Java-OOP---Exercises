d02bb64a43e60ba38223d3b84ff9890226ddea97
Huy Nguyen Pham Quoc <intern.npqhuy@tma.com.vn>
2022-04-18 11:01:58 +0700

    update exercise 1

--- src/main/exercise1/Display.java
+++ src/main/exercise1/Display.java
@@ -3,4 +3,25 @@ package main.exercise1;
 public class Display {
     private float size;
     private int numberOfColors;
+
+    public Display(float size, int numberOfColors) {
+        this.size = size;
+        this.numberOfColors = numberOfColors;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public int getNumberOfColors() {
+        return numberOfColors;
+    }
+
+    public void setNumberOfColors(int numberOfColors) {
+        this.numberOfColors = numberOfColors;
+    }
 }
--- src/test/exercise1/GSMTest.java
+++ src/test/exercise1/GSMTest.java
@@ -1,14 +1,20 @@
 package test.exercise1;
 
+import main.exercise1.Battery;
+import main.exercise1.Display;
 import main.exercise1.GSM;
 
 public class GSMTest {
     public static void main(String[] args) {
-        GSM gsm1 = new GSM("GSM1", "GSM");
-        GSM gsm2 = new GSM("GSM2", "GSM");
-        GSM gsm3 = new GSM("GSM3", "GSM");
-        GSM gsm4 = new GSM("GSM4", "GSM");
-        GSM gsm5 = new GSM("GSM5", "GSM");
+        Battery b1 = new Battery("BAT1", 100, 150);
+        Display d1 = new Display(5.5f, 256);
+        Battery b2 = new Battery("BAT2", 150, 200);
+        Display d2 = new Display(6f, 512);
+        GSM gsm1 = new GSM("GSM1", "GSM", 500, "GSM", b1, d1);
+        GSM gsm2 = new GSM("GSM2", "GSM", 600, "GSM", b2, d2);
+        GSM gsm3 = new GSM("GSM3", "GSM", 550, "GSM", b1, d2);
+        GSM gsm4 = new GSM("GSM4", "GSM", 525, "GSM", b2, d1);
+        GSM gsm5 = new GSM("GSM5", "GSM", 515, "GSMA", b1, d1);
 
 
         GSM[] GSMs = {gsm1, gsm2, gsm3};
